6128f3a90cebbbb6d81cf0f5478e11f166cff853
liufl <liufulai@wangfujing.com>
2016-04-25 18:10:39 +0800

    brand warm up optimization

--- online-index/src/main/java/com/wfj/search/online/index/operation/BrandCacheWarmUpOperation.java
+++ online-index/src/main/java/com/wfj/search/online/index/operation/BrandCacheWarmUpOperation.java
@@ -1,12 +1,28 @@
 package com.wfj.search.online.index.operation;
 
+import com.google.common.collect.Lists;
+import com.wfj.platform.util.analysis.Timer;
 import com.wfj.search.online.common.pojo.BrandPojo;
 import com.wfj.search.online.index.iao.IPcmRequester;
+import com.wfj.search.online.index.iao.IndexException;
+import com.wfj.search.online.index.iao.RequestException;
+import com.wfj.search.online.index.pojo.ItemIndexPojo;
+import com.wfj.search.online.index.service.IIndexConfigService;
+import com.wfj.search.online.index.util.ExecutorServiceFactory;
 import com.wfj.search.util.record.pojo.Operation;
+import org.apache.solr.common.SolrInputDocument;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.time.Duration;
+import java.util.Collections;
 import java.util.List;
+import java.util.concurrent.CompletionService;
+import java.util.concurrent.ExecutorCompletionService;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.atomic.AtomicReference;
 
 /**
  * 品牌缓存预热操作
@@ -17,18 +33,49 @@ import java.util.List;
  */
 @Component("brandCacheWarmUpOperation")
 public class BrandCacheWarmUpOperation implements IOperation<Void> {
+    private final Logger logger = LoggerFactory.getLogger(getClass());
     private boolean running = false;
     @Autowired
     private IPcmRequester pcmRequester;
+    @Autowired
+    private IIndexConfigService indexConfigService;
 
     @Override
     public Void operate(Operation operation) throws Exception {
         try {
             running = true;
             List<BrandPojo> brandPojos = pcmRequester.listBrands();
+            logger.debug("brand to warm-up counts: {}", brandPojos.size());
+            Timer timer = new Timer();
+            timer.start();int threads = this.indexConfigService.getFetchThreads();
+            final AtomicReference<Throwable> tracker = new AtomicReference<>();
+            ExecutorService threadPool = ExecutorServiceFactory.create("rebuildES", threads,
+                    Thread.currentThread(), tracker);
+            CompletionService<Void> completionService = new ExecutorCompletionService<>(threadPool);
             for (BrandPojo brandPojo : brandPojos) {
-                pcmRequester.directGetBrandInfo(brandPojo.getBrandId());
+                completionService.submit(() -> {
+                    Timer timer1 = new Timer();
+                    timer1.start();
+                    try {
+                        pcmRequester.directGetBrandInfo(brandPojo.getBrandId());
+                    } catch (RequestException e) {
+                        logger.warn("warm-up brand[{}] failed", brandPojo.getBrandId());
+                    }
+                    Duration stop = timer1.stop();
+                    logger.debug("brand[{}] warm-up, cost {}", brandPojo.getBrandId(), stop.toString());
+                }, null);
             }
+            try {
+                for (int i = 0; i < brandPojos.size(); i++) {
+                    completionService.take();
+                }
+            } catch (InterruptedException e) {
+                Throwable throwable = tracker.get();
+                throw new RuntimeException(e.toString(), throwable);
+            }
+            timer.stop();
+            Duration duration = Duration.between(timer.getStartTime(), timer.lastStop().getStopTime());
+            logger.info("warm-up {} brands, cost {}", brandPojos.size(), duration.toString());
             return null;
         } finally {
             running = false;
---
+++ online-index/src/test/java/com/wfj/search/online/index/operation/BrandCacheWarmUpOperationTest.java
@@ -0,0 +1,61 @@
+package com.wfj.search.online.index.operation;
+
+import com.wfj.platform.util.analysis.Timer;
+import com.wfj.search.util.record.pojo.Operation;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mock.jndi.SimpleNamingContextBuilder;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.sql.Timestamp;
+import java.time.Duration;
+
+/**
+ * <p>create at 16-4-25</p>
+ *
+ * @author liufl
+ * @since 1.0.35
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"classpath*:spring-test.xml"})
+public class BrandCacheWarmUpOperationTest {
+    private final Logger logger = LoggerFactory.getLogger(getClass());
+    @Autowired
+    @Qualifier("brandCacheWarmUpOperation")
+    private IOperation<Void> brandCacheWarmUpOperation;
+    @Value("${monitor.register.appName}")
+    private String appName;
+    @Autowired
+    @Qualifier("instanceName")
+    private String instanceName;
+
+    @BeforeClass
+    public static void setUpBeforeClass() throws Exception {
+        SimpleNamingContextBuilder jndiContextBuilder = SimpleNamingContextBuilder.emptyActivatedContextBuilder();
+        jndiContextBuilder.bind("service/address", "127.0.0.1:8080");
+        jndiContextBuilder.bind("service/instance", "indexServiceTest");
+    }
+
+    @Test
+    public void testOperate() throws Throwable {
+        Operation opRecord = new Operation();
+        opRecord.setAppName(this.appName);
+        opRecord.setInstanceName(this.instanceName);
+        opRecord.setStartTime(new Timestamp(System.currentTimeMillis()));
+        opRecord.setOperation("BRAND_WARM_UP");
+        opRecord.setParameter("");
+        opRecord.setCaller("SYS_INDEX_SERVICE");
+        Timer timer = new Timer();
+        timer.start();
+        brandCacheWarmUpOperation.operate(opRecord);
+        Duration stop = timer.stop();
+        logger.info("brand warm up costs: {}", stop.toString());
+    }
+}
