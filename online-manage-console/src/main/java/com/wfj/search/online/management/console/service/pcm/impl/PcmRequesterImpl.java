b41c546d7bfdcf4c1a530035c43c6eaaa9b25a1e
liufl <liufulai@wangfujing.com>
2016-05-24 17:19:00 +0800

    优化OkHttp代码

--- online-manage-console/src/main/java/com/wfj/search/online/management/console/service/pcm/impl/PcmRequesterImpl.java
+++ online-manage-console/src/main/java/com/wfj/search/online/management/console/service/pcm/impl/PcmRequesterImpl.java
@@ -5,17 +5,17 @@ import com.alibaba.fastjson.JSONObject;
 import com.google.common.collect.Lists;
 import com.wfj.search.online.common.pojo.CategoryPojo;
 import com.wfj.search.online.management.console.service.pcm.IPcmRequester;
-import okhttp3.*;
+import com.wfj.search.utils.http.OkHttpOperator;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.Validate;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
-import java.util.concurrent.TimeUnit;
 
 /**
  * <br/>create at 15-8-21
@@ -27,6 +27,8 @@ import java.util.concurrent.TimeUnit;
 @Service("pcmRequester")
 public class PcmRequesterImpl implements IPcmRequester {
     private final Logger logger = LoggerFactory.getLogger(getClass());
+    @Autowired
+    private OkHttpOperator okHttpOperator;
     @Value("${pcm.address}")
     private String address;
     @Value("${pcm.uri.listBrands}")
@@ -41,23 +43,16 @@ public class PcmRequesterImpl implements IPcmRequester {
     private String urlListChannels;
     @Value("${config.default.channel}")
     private String defaultChannel;
-    private OkHttpClient okHttpClient = new OkHttpClient.Builder().readTimeout(30, TimeUnit.SECONDS).build();
-    private MediaType mediaTypeJson = MediaType.parse("application/json; charset=utf-8");
 
     @Override
     public JSONArray listBrands() {
+        //noinspection Duplicates
         try {
-            Request request = new Request.Builder().url(address + "/" + listBrandUri)
-                    .post(RequestBody.create(mediaTypeJson, "{}")).build();
-            Response response = okHttpClient.newCall(request).execute();
-            JSONObject jsonObjectGet;
-            if (response.isSuccessful()) {
-                String respStr = response.body().string();
-                jsonObjectGet = JSONObject.parseObject(respStr);
-            } else {
-                throw new RuntimeException("请求PCM查询品牌列表返回非2xx响应");
-            }
-            Boolean success = jsonObjectGet.getBoolean("success");
+            final String url = address + "/" + listBrandUri;
+            final String jsonText = "{}";
+            final String respStr = this.okHttpOperator.postJsonTextForTextResp(url, jsonText);
+            final JSONObject jsonObjectGet = JSONObject.parseObject(respStr);
+            final Boolean success = jsonObjectGet.getBoolean("success");
             if (success != null && success) {
                 return jsonObjectGet.getJSONArray("list");
             }
@@ -69,30 +64,25 @@ public class PcmRequesterImpl implements IPcmRequester {
 
     @Override
     public List<CategoryPojo> listCategoryByChannel(String channel) {
-        List<CategoryPojo> categories = Lists.newArrayList();
+        final List<CategoryPojo> categories = Lists.newArrayList();
         try {
-            String url = address + "/" + uriListCategoryByChannel;
-            JSONObject jsonBody = new JSONObject();
+            final String url = address + "/" + uriListCategoryByChannel;
+            final JSONObject jsonBody = new JSONObject();
             if (StringUtils.isNotBlank(channel)) {
                 jsonBody.put("channelCode", channel);
             } else {
                 jsonBody.put("channelCode", defaultChannel);
             }
-            JSONObject jsonObjectGet;
-            RequestBody requestBody = RequestBody.create(mediaTypeJson, jsonBody.toJSONString());
-            Request request = new Request.Builder().url(url).post(requestBody).build();
-            Response response = okHttpClient.newCall(request).execute();
-            if (response.isSuccessful()) {
-                String respStr = response.body().string();
-                jsonObjectGet = JSONObject.parseObject(respStr);
-            } else {
-                throw new RuntimeException("请求PCM查询渠道下分类列表返回非2xx响应");
-            }
-            Boolean success = jsonObjectGet.getBoolean("success");
+            final String jsonText = jsonBody.toJSONString();
+            final String respStr = this.okHttpOperator.postJsonTextForTextResp(url, jsonText);
+            final JSONObject jsonObjectGet = JSONObject.parseObject(respStr);
+            final Boolean success = jsonObjectGet.getBoolean("success");
             if (success != null && success) {
-                JSONArray categoryJsonArray = jsonObjectGet.getJSONArray("list");
-                for (int i = 0; i < categoryJsonArray.size(); i++) {
-                    getCategoryInfo(categoryJsonArray.getString(i)).ifPresent(categories::add);
+                final JSONArray categoryJsonArray = jsonObjectGet.getJSONArray("list");
+                final int size = categoryJsonArray.size();
+                for (int i = 0; i < size; i++) {
+                    final String categoryId = categoryJsonArray.getString(i);
+                    getCategoryInfo(categoryId).ifPresent(categories::add);
                 }
             }
         } catch (Exception e) {
@@ -103,29 +93,24 @@ public class PcmRequesterImpl implements IPcmRequester {
 
     @Override
     public List<CategoryPojo> listDisplayCategoryChildren(String parentId) {
-        List<CategoryPojo> categories = Lists.newArrayList();
+        final List<CategoryPojo> categories = Lists.newArrayList();
         try {
-            String url = address + "/" + uriListSubCategories;
-            JSONObject jsonBody = new JSONObject();
+            final String url = address + "/" + uriListSubCategories;
+            final JSONObject jsonBody = new JSONObject();
             if (StringUtils.isNotBlank(parentId)) {
                 jsonBody.put("categoryId", parentId);
             }
-            JSONObject jsonObjectGet;
-            RequestBody requestBody = RequestBody.create(mediaTypeJson, jsonBody.toJSONString());
-            Request request = new Request.Builder().url(url).post(requestBody).build();
-            Response response = okHttpClient.newCall(request).execute();
-            if (response.isSuccessful()) {
-                String respStr = response.body().string();
-                jsonObjectGet = JSONObject.parseObject(respStr);
-            } else {
-                throw new RuntimeException("请求PCM查询子级分类列表返回非2xx响应");
-            }
-            Boolean success = jsonObjectGet.getBoolean("success");
+            final String jsonText = jsonBody.toJSONString();
+            final String respStr = this.okHttpOperator.postJsonTextForTextResp(url, jsonText);
+            final JSONObject jsonObjectGet = JSONObject.parseObject(respStr);
+            final Boolean success = jsonObjectGet.getBoolean("success");
             if (success != null && success) {
-                JSONArray categoryJsonArray = jsonObjectGet.getJSONArray("list");
-                for (int i = 0; i < categoryJsonArray.size(); i++) {
-                    JSONObject categoryInfo = categoryJsonArray.getJSONObject(i);
-                    getCategoryInfo(categoryInfo.getString("categoryId")).ifPresent(categories::add);
+                final JSONArray categoryJsonArray = jsonObjectGet.getJSONArray("list");
+                final int size = categoryJsonArray.size();
+                for (int i = 0; i < size; i++) {
+                    final JSONObject categoryInfo = categoryJsonArray.getJSONObject(i);
+                    final String categoryId = categoryInfo.getString("categoryId");
+                    getCategoryInfo(categoryId).ifPresent(categories::add);
                 }
             }
         } catch (Exception e) {
@@ -137,25 +122,18 @@ public class PcmRequesterImpl implements IPcmRequester {
     @Override
     public Optional<CategoryPojo> getCategoryInfo(String categoryId) {
         try {
-            String url = address + "/" + uriGetCategoryInfo;
-            JSONObject jsonBody = new JSONObject();
+            final String url = address + "/" + uriGetCategoryInfo;
+            final JSONObject jsonBody = new JSONObject();
             if (StringUtils.isNotBlank(categoryId)) {
                 jsonBody.put("categoryId", categoryId);
             }
-            JSONObject jsonObjectGet;
-            RequestBody requestBody = RequestBody.create(mediaTypeJson, jsonBody.toJSONString());
-            Request request = new Request.Builder().url(url).post(requestBody).build();
-            Response response = okHttpClient.newCall(request).execute();
-            if (response.isSuccessful()) {
-                String respStr = response.body().string();
-                jsonObjectGet = JSONObject.parseObject(respStr);
-            } else {
-                throw new RuntimeException("请求PCM查询分类信息返回非2xx响应");
-            }
-            Boolean success = jsonObjectGet.getBoolean("success");
+            final String jsonText = jsonBody.toJSONString();
+            final String respStr = this.okHttpOperator.postJsonTextForTextResp(url, jsonText);
+            final JSONObject jsonObjectGet = JSONObject.parseObject(respStr);
+            final Boolean success = jsonObjectGet.getBoolean("success");
             if (success != null && success) {
-                CategoryPojo categoryPOJO = new CategoryPojo();
-                JSONObject categoryInfo = jsonObjectGet.getJSONObject("item");
+                final CategoryPojo categoryPOJO = new CategoryPojo();
+                final JSONObject categoryInfo = jsonObjectGet.getJSONObject("item");
                 categoryPOJO.setCategoryId(Validate.notBlank(categoryInfo.getString("categoryId"), "分类编码为空").trim());
                 categoryPOJO
                         .setCategoryName(Validate.notBlank(categoryInfo.getString("categoryName"), "分类名称为空").trim());
@@ -166,7 +144,8 @@ public class PcmRequesterImpl implements IPcmRequester {
                 categoryPOJO.setParentCategoryId(categoryInfo.getString("parentCategoryId"));
                 categoryPOJO.setLevel(Validate.notNull(categoryInfo.getInteger("level"), "分类Level为空"));
                 categoryPOJO.setOrder(Validate.notNull(categoryInfo.getInteger("order"), "同级分类序号为空"));
-                categoryPOJO.setChannel(Validate.notBlank(categoryInfo.getString("channel"), "分类所属销售渠道编码为空").trim());
+                categoryPOJO
+                        .setChannel(Validate.notBlank(categoryInfo.getString("channel"), "分类所属销售渠道编码为空").trim());
                 return Optional.of(categoryPOJO);
             }
         } catch (Exception e) {
@@ -177,19 +156,13 @@ public class PcmRequesterImpl implements IPcmRequester {
 
     @Override
     public JSONArray listChannels() {
+        //noinspection Duplicates
         try {
-            String url = address + "/" + urlListChannels;
-            JSONObject jsonObjectGet;
-            Request request = new Request.Builder().url(url)
-                    .post(RequestBody.create(mediaTypeJson, "{}")).build();
-            Response response = okHttpClient.newCall(request).execute();
-            if (response.isSuccessful()) {
-                String respStr = response.body().string();
-                jsonObjectGet = JSONObject.parseObject(respStr);
-            } else {
-                throw new RuntimeException("请求PCM查询渠道列表信息返回非2xx响应");
-            }
-            Boolean success = jsonObjectGet.getBoolean("success");
+            final String url = address + "/" + urlListChannels;
+            final String jsonText = "{}";
+            final String respStr = this.okHttpOperator.postJsonTextForTextResp(url, jsonText);
+            final JSONObject jsonObjectGet = JSONObject.parseObject(respStr);
+            final Boolean success = jsonObjectGet.getBoolean("success");
             if (success != null && success) {
                 return jsonObjectGet.getJSONArray("list");
             }
